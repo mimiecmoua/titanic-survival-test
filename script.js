7260018de6a27c46802f0c3cd4d6cf75b2c30aff
Emilie <dionatsumi@gmail.com>
2025-02-13 10:11:28 +0100

    update URL API for deployment

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         try {
             // 🔹 2. Envoyer la requête POST à Flask
-            const response = await fetch("http://127.0.0.1:5000/predict", {
+            const response = await fetch("https://titanic-api-6g6t.onrender.com/predict", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
